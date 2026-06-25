fd9657aa108f64c6ef6cac105795b75900ba0b28
Rikhav <rpshah@umass.edu>
2026-06-24 23:58:31 -0400

    updated comments for problem

--- Arrays_and_Hasing/217-containsDuplicates.py
+++ Arrays_and_Hasing/217-containsDuplicates.py
@@ -1,5 +1,15 @@
-#Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+"""
+Problem: LeetCode 217 - Contains Duplicate
 
+Intuition:
+To check if there are any duplicates in the given list, we can use a hash set (set in Python) to store the unique elements as we traverse the list. For each element, we check if it is already present in the set. If it is, then we have found a duplicate, and we return True. If we traverse the entire list without finding any duplicates, we return False.
+
+Time Complexity:
+O(n) - we iterate over every element once at worst
+
+Space Complexity:
+O(n) - we store every number once at worst 
+"""
 
 class Solution:
     def hasDuplicate(self, nums: list[int]) -> bool:
